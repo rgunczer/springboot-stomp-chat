ddcbf48127f440133ec2f065f77680212259e954
rgunczer <rgunczer@gmail.com>
2023-02-13 03:36:03 +0100

    rename app

--- src/main/java/com/example/demo/StompChatApplication.java
+++ src/main/java/com/example/demo/StompChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class StompChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(StompChatApplication.class, args);
 	}
 
 }
